884ff746971be3ce93a832e5968191956aa73fa6
Kc <kifeakor@yahoo.com>
2023-01-27 16:27:19 +0000

    timeblocks update with time added to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
 let currentDayElement = $("#currentDay")
 let timeInterval;
+let timeBlockElement = $(".time-block")
+
+
+
+let updateTimeBlockColor = function () {
+    let currentTime = moment();
+    
+    timeBlockElement.each(function() {
+        let timeBlockTime = moment($(this).data("time"), "hh:mm a");
+        let nextHour = timeBlockTime.clone().add(1, 'hours');
+        console.log(this)
+        console.log(timeBlockTime)
+        if(currentTime.isBefore(timeBlockTime)) {
+            $(this).addClass("future");
+        }
+        else if(currentTime.isBetween(timeBlockTime, nextHour)) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    });
+}
+
+//call this function to initialize the color of the time blocks
+updateTimeBlockColor();
+
+// set an interval to update the color of the time blocks every minute
+timeInterval = setInterval(updateTimeBlockColor, 3600000);
+
+    
+
+
+
+
+
+
+
 
 // get time function
 let startTime = function(){
